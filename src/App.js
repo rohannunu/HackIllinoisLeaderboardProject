4d980b9554e59bd5415d9eb2b7d3dcd659faffad
Rohan Nunugonda <rohannunu@gmail.com>
2023-07-12 02:36:07 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
+/*Tutorials and Documentation Used:
+1) https://www.codingthesmartway.com/how-to-fetch-api-data-with-react/ 
+  Specific Hook Documentation:
+    1) useState Hook: https://react.dev/reference/react/useState
+      a) Summary: useState allows us to  
+    2) useEffect Hook: 
+
+
+    2) Helped me dynamically number the list: https://stackoverflow.com/questions/62840731/add-numbers-to-list-item-react-native
+*/
+
+import 'bootstrap/dist/css/bootstrap.css';
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react" //Using the useEffect and useState Hook
+
 
 function App() {
+
+  const [users, setUsers] = useState([])
+  
+
+  const fetchUserData = () => {
+    fetch(`https://api.hackillinois.org/profile/leaderboard/`)
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      setUsers(data.profiles)
+    })
+    console.log(users)
+  }
+
+  useEffect(() => {
+    fetchUserData()
+  }, [])
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <body id="gradient">
+    <h1> HackIllinois Leaderboard </h1>
+      {(
+        <div id="largeContainer">
+
+          <div id="containerTop3">
+            {users.slice(0, 3).map((user, index) => (
+            <div class="card" style={{ width: "18rem;" }}>
+            <div class="card-body">
+              <h5 class="card-title">{index + 1}: {user.discord}</h5>
+              <p class="card-text">Points: {user.points}</p>
+            </div>
+          </div>
+          ))}</div>
+
+          <div id="containerRemaining">
+            {users.slice(3).map((user, index) => (
+            <div class="card" style={{ width: "18rem;" }}>
+            <div class="card-body">
+              <h5 class="card-title">{index + 4}: {user.discord}</h5>
+              <p class="card-text">Points: {user.points}</p>
+            </div>
+          </div>
+          ))}</div>
+
+          
+       
+        </div>
+      )}
+      </body>
+    </>
   );
 }
 
